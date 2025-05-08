daa7b5f63883f7a00becb1c795a9b9c1091b84b5
Bryan Herdianto <bryan.herdianto17@gmail.com>
2025-05-08 22:47:18 +0700

    fix flickering navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-black">
-        <div className="max-w-[1440px] mx-auto relative">
-          <div className="bg-center -z-1 fixed top-0 w-screen h-screen bg-cover" style={{ backgroundImage: "url('/background.svg')" }}></div>
+      <body>
+        <div className="max-w-[1440px] mx-auto relative bg-black">
+          <div className="bg-center z-1 fixed top-0 w-screen h-screen bg-cover" style={{ backgroundImage: "url('/background.svg')" }}></div>
           <div className="relative z-10">
             {children}
           </div>
