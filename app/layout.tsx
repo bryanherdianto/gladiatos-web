a83371899068933d4bdc98b67836a53436c941a8
Bryan Herdianto <bryan.herdianto17@gmail.com>
2025-05-09 00:12:15 +0700

    pls 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Gladiatos UI",
   description: "Gladiatos from Tim Robotika Universitas Indonesia",
-  icons: "/icon_web_gladiatos.svg",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
-  themeColor: "#000000",
+  icons: "/icon_web_gladiatos.svg"
 };
 
 export default function RootLayout({
@@ -21,9 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-black">
+      <head>
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body>
-        <div className="max-w-[1440px] mx-auto relative">
-          <div className="bg-center z-1 fixed top-0 w-screen h-screen bg-cover" style={{ backgroundImage: "url('/background.svg')" }}></div>
+        <div className="max-w-[1440px] mx-auto relative bg-black">
+          <div className="bg-center z-1 fixed top-0 w-screen h-screen bg-cover bg-black" style={{ backgroundImage: "url('/background.svg')" }}></div>
           <div className="relative z-10">
             {children}
           </div>
